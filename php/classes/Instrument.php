b9cb9416eaed8859b981742b92faa5f618a5c87c
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-31 08:58:16 -0600

    Added more accessors and mutators

--- php/classes/Instrument.php
+++ php/classes/Instrument.php
@@ -2,15 +2,18 @@
 
 
 class Instrument {
-	private $instrumentName;
-	private $instrumentFamily;
-	private $instrumentDescription;
+	protected $instrumentName;
+	protected $instrumentFamily;
+	protected $instrumentDescription;
+	protected $instrumentHasOwner;
 
-	public function __construct(string $newInstrumentName, string $newInstrumentFamily, string $newInstrumentDescription ) {
+	public function __construct(string $newInstrumentName, string $newInstrumentFamily, string $newInstrumentDescription,
+											bool $newInstrumentHasOwner = false) {
 		try {
 			$this->setInstrumentName($newInstrumentName);
 			$this->setInstrumentFamily($newInstrumentFamily);
 			$this->setInstrumentDescription($newInstrumentDescription);
+			$this->setInstrumentHasOwner($newInstrumentHasOwner);
 		} catch(\InvalidArgumentException | \RangeException |\TypeError | \Exception $exception) {
 			//determine what exception type was thrown
 			$exceptionType = get_class($exception);
@@ -18,9 +21,32 @@ class Instrument {
 		}
 	}
 
+	public function getInstrumentName() : string {
+		return ($this->instrumentName);
+	}
 
+	public function setInstrumentName(string $newInstrumentName) {
+		//validate instrument name here
+		$this->instrumentName = $newInstrumentName;
+	}
 
+	public function getInstrumentFamily() : string {
+		return ($this->instrumentFamily);
+	}
 
+	public function setInstrumentFamily(string $newInstrumentFamily) {
+		//validate instrument family here
+		$this->instrumentFamily = $newInstrumentFamily;
+	}
+
+	public function getInstrumentDescription() : string {
+		return ($this->instrumentDescription);
+	}
+
+	public function setInstrumentDescription(string $newInstrumentDescription) {
+		//validate instrument description here
+		$this->instrumentDescription = $newInstrumentDescription;
+	}
 
 
 
